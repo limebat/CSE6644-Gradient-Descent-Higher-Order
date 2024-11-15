561f6235bdbd55dfae554d5d11eb498ac4af3e89
pipi-YJ <yzhang3988@gatech.edu>
2024-11-15 17:00:38 -0500

    Push 11/15

--- Main.py
+++ Main.py
@@ -9,23 +9,28 @@ def f(x, y, epsilon, alpha, m, n, eta):
     return func
  
 def f_grad(x, y, epsilon, alpha, m, n, eta):
+    h = x**(2 * np.max([n, m]) + epsilon) + y**(2 * np.max([n, m]) + epsilon)
     exp_coeff_max = 2 * np.max([n, m]) + epsilon
-    dhdx = exp_coeff_max * x**(exp_coeff_max - 1) 
-    dfuncdx = y*(2*n * x**(2*n - 1) + dhdx)
+    print('The exp coefficient is: ', exp_coeff_max)
+    dhdx = exp_coeff_max * x**(exp_coeff_max-1)
     dhdy = exp_coeff_max * y**(exp_coeff_max - 1) 
-    dfuncdy = x**(2*n) + 2*m * alpha / (2*m+1) * y**(2*m) + dhdy
+    dfuncdx = y*(2*n * x**(2*n - 1) + dhdx)
+    print('dhdy is the following value: ', dhdy)
+    dfuncdy = x**(2*n) + alpha* y**(2*m) + h + y*dhdy
+    print('This is the value of dfdy: ', dfuncdy)
     return dfuncdx, dfuncdy
 
+''' 
 def case1(x, y, epsilon, alpha, m, n, eta):
     h = x**(2 * m + epsilon) + y**(2 * m + epsilon)
     return h
-    
+
 def case2(x, y, epsilon, alpha, m, n, eta):
-    dhdx = x**(2 * m + epsilon) 
+    dhdx = (2 * m + epsilon) * x**(2 * m - 1 + epsilon) 
     return dhdx
 
 def case3(x, y, epsilon, alpha, m, n, eta):
-    dhdy = x**(2 * m + epsilon) + y**(2 * m - 1 + epsilon)
+    dhdy = (2 * m + epsilon)* y**(2 * m - 1 + epsilon)
     return dhdy
 
 def f_grad_case1(x, y, epsilon, alpha, m, n, eta):
@@ -51,8 +56,9 @@ def f_grad_case3(x, y, epsilon, alpha, m, n, eta):
     dhdy = case3(x, y, epsilon, alpha, m, n, eta) 
     dfuncdy = x**(2*n) + 2*m * alpha / (2*m+1) * y**(2*m) + dhdy
     return dfuncdx, dfuncdy
+'''
 
-def GD(x0, y0, epsilon, alpha, m, n, eta, tol=1e-4, max_iter=1e3):
+def GD(x0, y0, epsilon, alpha, m, n, eta, tol=1e-10, max_iter=1000):
     err = 1
     k  = 0
     x = [x0]
@@ -63,7 +69,9 @@ def GD(x0, y0, epsilon, alpha, m, n, eta, tol=1e-4, max_iter=1e3):
         x.append(x[k] - eta * dfuncdx) 
         y.append(y[k] - eta * dfuncdy)
         
-        err = np.sqrt((x[k+1] - x[k])**2 + (y[k+1] - y[k])**2)
+        print(y[k])
+
+        err =   np.sqrt(x[k]*x[k] + y[k]*y[k])
         
         k += 1
         
@@ -74,17 +82,17 @@ def GD(x0, y0, epsilon, alpha, m, n, eta, tol=1e-4, max_iter=1e3):
         
     return x, y
 
-m = 2
-n = 2
+m = 3
+n = 1
 alpha = 2
-epsilons = [i * 0.1 for i in range(20)] #Range 0.1, 0.2, ... 1
-eta = 0.1
-x0 = 1
-y0 = 1
+epsilons = [1, 2] #[i * 0.1 for i in range(20)] #Range 0.1, 0.2, ... 1
+eta = 0.8
+x0 = 0.2
+y0 = 0.3
 dx = dy = 0.01
 
-x = np.arange(0, 1+dx, dx)
-y = np.arange(0, 1+dy, dy)
+x = np.arange(-(1+dx), 1+dx, dx)
+y = np.arange(-(1+dy), 1+dy, dy)
 X, Y = np.meshgrid(x, y)
 
 # Now solve for each epsilon -- order magnitude increase each time for each term for further incrases in power
@@ -101,13 +109,14 @@ for epsilon in epsilons:
 
 #Plot the x and y through each iteration
 
-plt.loglog(x, y)
+plt.plot(x, y)
 plt.title('Iterations of x and y throughout respective iterations')
 plt.xlabel(f'$x_k$')
 plt.ylabel(f'$y_k$')
 plt.grid(True, which='both', ls='--')
 
 plt.show()
+
 
 #Now plot its change w.r.t epsilon
 fig = plt.figure()
@@ -124,7 +133,7 @@ plt.show()
 
 dfuncdx, dfuncdy = f_grad(X, Y, epsilon, alpha, m, n, eta)
 
-plt.quiver(X, Y, -dfuncdx, -dfuncdy)
+plt.streamplot(X,Y,-dfuncdx,-dfuncdy)
 
 plt.title('Gradient Field Visualization, General Function', fontsize=16)
 plt.xlabel('X-axis', fontsize=12)
@@ -136,13 +145,13 @@ plt.ylim([Y.min(), Y.max()])
 
 plt.show()
 
-
+'''
 #Sketch function: Case 1
 
 dfuncdx, dfuncdy = f_grad_case1(X, Y, epsilon, alpha, m, n, eta)
 
-plt.quiver(X, Y, -dfuncdx, -dfuncdy)
 
+plt.streamplot(X,Y,-dfuncdx,-dfuncdy)
 plt.title('Gradient Field Visualization, Case 1', fontsize=16)
 plt.xlabel('X-axis', fontsize=12)
 plt.ylabel('Y-axis', fontsize=12)
@@ -157,8 +166,8 @@ plt.show()
 
 dfuncdx, dfuncdy = f_grad_case2(X, Y, epsilon, alpha, m, n, eta)
 
-plt.quiver(X, Y, -dfuncdx, -dfuncdy)
 
+plt.streamplot(X,Y,-dfuncdx,-dfuncdy)
 plt.title('Gradient Field Visualization, Case 2', fontsize=16)
 plt.xlabel('X-axis', fontsize=12)
 plt.ylabel('Y-axis', fontsize=12)
@@ -171,7 +180,7 @@ plt.show()
 
 dfuncdx, dfuncdy = f_grad_case3(X, Y, epsilon, alpha, m, n, eta)
 
-plt.quiver(X, Y, -dfuncdx, -dfuncdy)
+plt.streamplot(X,Y,-dfuncdx,-dfuncdy)
 
 plt.title('Gradient Field Visualization, Case 3', fontsize=16)
 plt.xlabel('X-axis', fontsize=12)
@@ -182,3 +191,4 @@ plt.xlim([X.min(), X.max()])
 plt.ylim([Y.min(), Y.max()])
 
 plt.show()
+'''
